7a4d221c8b2f65f2199261d412abd36b69321b91
dreamhandg <handg0972@gmail.com>
2022-08-01 19:59:56 +0900

    first commit

---
+++ src/main/java/com/websocket/edu/SpringWebSocketEduApplication.java
@@ -0,0 +1,13 @@
+package com.websocket.edu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebSocketEduApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebSocketEduApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/websocket/edu/SpringWebSocketEduApplicationTests.java
@@ -0,0 +1,13 @@
+package com.websocket.edu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebSocketEduApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
